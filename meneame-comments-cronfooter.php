408d3bd69c3ebd8cc0c1f5d501500de46587feb2
blogestudio <blogestudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-25 17:28:49 +0000

    uploading trunk directory
    git-svn-id: https://plugins.svn.wordpress.org/meneame-comments-to-wp/trunk@70464 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- meneame-comments-cronfooter.php
+++ meneame-comments-cronfooter.php
@@ -4,17 +4,14 @@
 function meneame_comments__wp_footer() {
 	global $posts, $meneamec_url;
 	
-	if ( is_single() ) {
-		
-		$single_cron = meneame_comments__get_option('single_cron');
-		
-		$post = $posts[0];
-		
-		if ( $single_cron && isset($single_cron[$post->ID]) ) {
-			echo "\n";
-				echo '<script type="text/javascript" src="'.$meneamec_url.'/meneame-comments-upload-js.php?mc_pid='.$post->ID.'"></script>';
-			echo "\n";
-		}
+	$single_cron = meneame_comments__get_option('single_cron');
+	
+	$post = $posts[0];
+	
+	if ( $single_cron && isset($single_cron[$post->ID]) ) {
+		echo "\n";
+			echo '<script type="text/javascript" src="'.$meneamec_url.'/meneame-comments-upload-js.php?mc_pid='.$post->ID.'"></script>';
+		echo "\n";
 	}
 }
 add_action('wp_footer', 'meneame_comments__wp_footer');
