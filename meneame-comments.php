e2b0754abd3c95c29960c54f7c09d2e6e96a7608
blogestudio <blogestudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-20 11:40:13 +0000

    tagging version 0.0.13
    git-svn-id: https://plugins.svn.wordpress.org/meneame-comments-to-wp/trunk@74929 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- meneame-comments.php
+++ meneame-comments.php
@@ -5,7 +5,7 @@ Plugin URI: http://blogestudio.com/plugins/meneame-comments/
 Description: Automatic system to obtain the comments of your entries in Meneame
 Author: Alejandro Carravedo (Blogestudio)
 Author URI: http://blogestudio.com/
-Version: 0.0.12
+Version: 0.0.13
 Date: 2008-10-25 19:00:00
 */
 
